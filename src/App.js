ebd9717e4d806c5ae52ece1fdfbe84545e171976
Yongnam Kim <yong4@hawaii.edu>
2020-09-19 13:23:00 -0400

    complete navs section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Main from './main/Main';
+import Navs from './navs/Navs';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App fulldemention">
+      <div className="Banner">
+
+      </div>
+      <div className="nav">
+        <Navs/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
@@ -16,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <Main/>
       </header>
     </div>
   );
---
+++ src/main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+    return (
+        <div class="container-fluid">
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-light">Light</button>
+            <button type="button" class="btn btn-dark">Dark</button>
+
+            <button type="button" class="btn btn-link">Link</button>
+        </div>
+    );
+}
+
+export default Main;
