4f34433e0b161ec61fb9e58aedc0f29c9f5cb0f3
Yongnam Kim <yong4@hawaii.edu>
2020-10-12 17:31:37 -0400

    complete nav section

--- src/App.js
+++ src/App.js
@@ -10,21 +10,12 @@ function App() {
       <div className="Banner">
         <h1 className="banner-title">Welcome to Yongshine.info</h1>
       </div>
-      <div className="nav">
+      <div className="nav-section">
         <Navs />
       </div>
-      <header className="App-header">
-        <h1>Hello from V2</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="main">
         <Main />
-      </header>
+      </div>
     </div>
   );
 }
--- src/navs/Navs.js
+++ src/navs/Navs.js
@@ -3,21 +3,31 @@ import './Navs.scss';
 
 function Navs() {
     return (
-        <div class="container-fluid">
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
+        <div class="card text-center">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Photos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Bio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
     );
 }
