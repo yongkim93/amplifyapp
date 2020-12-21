703bb238455aec284a72be55dd9c8bc820baa3e6
Yongnam Kim <yong4@hawaii.edu>
2020-12-21 15:28:28 -0500

    its working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import logo from './logo.svg';
 import Navs from './navs/Navs';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App fulldemention">
-      <div className="Banner">
-        <h1 className="banner-title">Welcome to Yongshine.info</h1>
+class App extends React.Component {
+
+  componentDidMount() {
+    const semanticJs = document.createElement("script");
+    const semanticCss = document.createElement("link");
+    semanticCss.rel = "stylesheet";
+    semanticCss.href = "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css";
+    semanticJs.src = "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js";
+    semanticJs.async = true;
+
+    document.body.appendChild(semanticJs);
+    document.body.appendChild(semanticCss);
+  }
+
+  render() {
+    return (
+      <div className="App fulldemention">
+        <div className="Banner">
+          <h1 className="banner-title">Welcome to Yongshine.info</h1>
+        </div>
+        <div className="nav-section">
+          <Navs />
+        </div>
+        <div className="main">
+        </div>
       </div>
-      <div className="nav-section">
-        <Navs />
-      </div>
-      <div className="main">
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/navs/Navs.js
+++ src/navs/Navs.js
@@ -4,31 +4,44 @@ import Main from '../main/Main';
 
 function Navs() {
     return (
-        <div class="card text-center">
-            <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
+        <div className="card text-center">
+            <div className="card-header">
+                <ul className="nav nav-tabs card-header-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Photos</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#" onClick={httpReq}>Photos</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Bio</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Bio</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Blog</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Contact</a>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Contact</a>
                     </li>
                 </ul>
             </div>
-            <div class="card-body">
+            <div className="card-body">
                 <Main />
             </div>
         </div>
     );
+}
+function httpReq() {
+    const Http = new XMLHttpRequest();
+    const url = 'https://vp9rxx6qx3.execute-api.us-east-1.amazonaws.com/prod/';
+    Http.open("POST", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        const res = JSON.parse(Http.response);
+        console.log(res);
+        const imgs = res.Contents.map(img => img.Key.replace('yong/', ''));
+        console.log(imgs);
+    }
 }
 
 export default Navs;
